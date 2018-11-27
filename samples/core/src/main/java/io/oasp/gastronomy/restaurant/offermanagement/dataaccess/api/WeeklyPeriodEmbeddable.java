f71f42bc50f1fa35f794f5ae882e79d42dd54515
mnowacki07 <218256@student.pwr.edu.pl>
2018-11-27 12:04:21 +0100

    Added: Special Offers entity

---
+++ samples/core/src/main/java/io/oasp/gastronomy/restaurant/offermanagement/dataaccess/api/WeeklyPeriodEmbeddable.java
@@ -0,0 +1,91 @@
+package io.oasp.gastronomy.restaurant.offermanagement.dataaccess.api;
+
+import java.time.DayOfWeek;
+
+import javax.persistence.Embeddable;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
+/**
+ * @author MNOWACKI
+ *
+ */
+@Embeddable
+public class WeeklyPeriodEmbeddable {
+
+  private DayOfWeek startingDay;
+
+  /**
+   * @return startingDay
+   */
+  public DayOfWeek getStartingDay() {
+
+    return this.startingDay;
+  }
+
+  /**
+   * @param startingDay new value of {@link #getstartingDay}.
+   */
+  public void setStartingDay(DayOfWeek startingDay) {
+
+    this.startingDay = startingDay;
+  }
+
+  /**
+   * @return startingHour
+   */
+  public int getStartingHour() {
+
+    return this.startingHour;
+  }
+
+  /**
+   * @param startingHour new value of {@link #getstartingHour}.
+   */
+  public void setStartingHour(int startingHour) {
+
+    this.startingHour = startingHour;
+  }
+
+  /**
+   * @return endingDay
+   */
+  public DayOfWeek getEndingDay() {
+
+    return this.endingDay;
+  }
+
+  /**
+   * @param endingDay new value of {@link #getendingDay}.
+   */
+  public void setEndingDay(DayOfWeek endingDay) {
+
+    this.endingDay = endingDay;
+  }
+
+  /**
+   * @return endingHour
+   */
+  public int getEndingHour() {
+
+    return this.endingHour;
+  }
+
+  /**
+   * @param endingHour new value of {@link #getendingHour}.
+   */
+  public void setEndingHour(int endingHour) {
+
+    this.endingHour = endingHour;
+  }
+
+  @Max(24)
+  @Min(0)
+  private int startingHour;
+
+  private DayOfWeek endingDay;
+
+  @Max(24)
+  @Min(0)
+  private int endingHour;
+}
